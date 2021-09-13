f833148d31b09c57cbcaaacf5da14d825d0b393c
Changhwan <ckdghks34@gmail.com>
2021-09-14 00:34:19 +0900

    upload RealtimeActivity

--- android/app/src/main/java/com/example/a10jobs/RealtimeActivity.java
+++ android/app/src/main/java/com/example/a10jobs/RealtimeActivity.java
@@ -1,6 +1,11 @@
 package com.example.a10jobs;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +15,33 @@ public class RealtimeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_realtime);
+
+        ImageButton btnUp = (ImageButton) findViewById(R.id.arrowkeys_up);
+        ImageButton btnDown = (ImageButton) findViewById(R.id.arrowkeys_down);
+        ImageButton btnLeft = (ImageButton) findViewById(R.id.arrowkeys_left);
+        ImageButton btnRight = (ImageButton) findViewById(R.id.arrowkeys_right);
+
+        btnUp.setOnTouchListener(onTouchListener);
+        btnDown.setOnTouchListener(onTouchListener);
+        btnLeft.setOnTouchListener(onTouchListener);
+        btnRight.setOnTouchListener(onTouchListener);
     }
+
+    // Touch 이벤트
+    ImageButton.OnTouchListener onTouchListener = new View.OnTouchListener(){
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            switch (motionEvent.getAction()){
+                case MotionEvent.ACTION_DOWN:   // 버튼이 눌렸을 때
+                case MotionEvent.ACTION_MOVE:   // 버튼이 눌려져 있을 때
+                    view.setBackgroundColor(Color.parseColor("#757575"));
+                    break;
+                case MotionEvent.ACTION_UP:     // 버튼 뗄 때
+                    view.setBackgroundColor(Color.TRANSPARENT);
+                    break;
+            }
+            return false;
+        }
+    };
+
 }
