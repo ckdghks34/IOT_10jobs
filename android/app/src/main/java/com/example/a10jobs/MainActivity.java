0e900dc6c0d5e54b870e2913446774f2728241ca
DESKTOP-KVCQHCD\multicampus <ehdqlaehdqls@gmail.com>
2021-09-14 11:26:04 +0900

    MainActivity, activity_main 충돌 지움

--- android/app/src/main/java/com/example/a10jobs/MainActivity.java
+++ android/app/src/main/java/com/example/a10jobs/MainActivity.java
@@ -6,14 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-<<<<<<< HEAD
 import android.widget.ImageButton;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import com.nex3z.notificationbadge.NotificationBadge;
-=======
->>>>>>> c420c327b67718b3a09a23c214a05a21f4a30e96
+
 
 public class MainActivity extends AppCompatActivity {
     Button btn_map, btn_control, btn_watch, btn_find, btn_status;
@@ -24,13 +22,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
-        btn_map = (Button)findViewById(R.id.btn_map);
-        btn_control = (Button)findViewById(R.id.btn_control);
-        btn_watch = (Button)findViewById(R.id.btn_watch);
-        btn_crime = (ToggleButton)findViewById(R.id.btn_crime);
-        btn_find = (Button)findViewById(R.id.btn_find);
-        btn_status = (Button)findViewById(R.id.btn_status);
+
+        btn_map = (Button) findViewById(R.id.btn_map);
+        btn_control = (Button) findViewById(R.id.btn_control);
+        btn_watch = (Button) findViewById(R.id.btn_watch);
+        btn_crime = (ToggleButton) findViewById(R.id.btn_crime);
+        btn_find = (Button) findViewById(R.id.btn_find);
+        btn_status = (Button) findViewById(R.id.btn_status);
         badge = findViewById(R.id.badge);
         badge.setNumber(2);
 
@@ -70,38 +68,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-=======
-    }
-
-
-    public void changeView(View view){
-//        Intent intent = null;
-
-        switch (view.getId()){
-            // 맵만들기 버튼 클릭시 화면 전환
-            case R.id.button3 :
-                break;
-            // 기기제어 버튼 클릭시 화면 전환
-            case R.id.button4 :
-                break;
-            // 실시간 보기 버튼 클릭시 화면 전환
-            case R.id.button5 :
-                Intent intent = new Intent(getApplicationContext(),RealtimeActivity.class);
-                startActivity(intent);
-                break;
-            // 방범모드 버튼 클릭시 화면 전환
-            case R.id.button6 :
-                break;
-            // 물건찾기 버튼 클릭시 화면 전환
-            case R.id.button7 :
-                break;
-            // 상태보기 버튼 클릭시 화면 전환
-            case R.id.button9 :
-                break;
-
-        }
-    }
-
->>>>>>> c420c327b67718b3a09a23c214a05a21f4a30e96
 }
+
