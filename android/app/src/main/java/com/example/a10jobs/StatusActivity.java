2bbdd90ca4e1c3d7316c56cab4ea2935c91d4ce4
jang930424@lab.ssffy.com <jang930424@naver.com>
2021-09-14 19:24:35 +0900

    Revert "장용원 status"
    This reverts commit c6e4031b69fecb5daef759aeede6e1ac6be4939a.

--- android/app/src/main/java/com/example/a10jobs/StatusActivity.java
+++ android/app/src/main/java/com/example/a10jobs/StatusActivity.java
@@ -1,11 +1,8 @@
 package com.example.a10jobs;
 
-import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
 
 public class StatusActivity extends AppCompatActivity {
 
@@ -13,11 +10,5 @@ public class StatusActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
-
-        Spinner spinner = (Spinner) findViewById(R.id.spinner_status);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.status, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
     }
 }
