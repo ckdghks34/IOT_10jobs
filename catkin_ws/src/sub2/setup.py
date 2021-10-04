6cedc996f98d526dc39a9816850ee17b6d3dc725
허창환 <ckdghks34@gmail.com>
2021-10-04 14:40:19 +0900

    upload wall_tracking need modification (Bed collision)

--- catkin_ws/src/sub2/setup.py
+++ catkin_ws/src/sub2/setup.py
@@ -28,7 +28,8 @@ setup(
             'odom = sub2.odom:main',
             'path_tracking = sub2.path_tracking:main',
             'seg_binarizer = sub2.seg_binarizer:main',
-            'avoid_obstacle = sub2.avoid_obstacle:main'
+            'avoid_obstacle = sub2.avoid_obstacle:main',
+            'wall_tracking = sub2.wall_tracking:main'
         ],
     },
 )
