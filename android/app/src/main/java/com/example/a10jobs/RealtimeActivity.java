1adcd85927f588baa1c59c8e188470b6c5a7f5db
DESKTOP-KVCQHCD\multicampus <ehdqlaehdqls@gmail.com>
2021-10-06 17:15:50 +0900

    [안드로이드] 실시간보기 버튼터치리스너 수정

--- android/app/src/main/java/com/example/a10jobs/RealtimeActivity.java
+++ android/app/src/main/java/com/example/a10jobs/RealtimeActivity.java
@@ -4,6 +4,8 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Base64;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -21,6 +23,7 @@ public class RealtimeActivity extends AppCompatActivity {
     ImageView realView;
     ImageButton btnUp, btnDown, btnLeft, btnRight;
     Bitmap bitmap_realTime;
+    boolean isBtnUp, isBtnDown, isBtnLeft, isBtnRight;
     String url = "http://j5d201.p.ssafy.io:12001";
     Socket socket;
     {
@@ -75,34 +78,185 @@ public class RealtimeActivity extends AppCompatActivity {
 //        Log.v("msg", "destroy 소켓 통신 해제");
     }
 
+    private void onBtnDown(){
+        TouchThread kThread = new TouchThread();
+        kThread.start();
+    }
+    private Handler touchHandler = new Handler()
+    {
+        public void handleMessage(Message msg)
+        {
+//            Log.d("MainActivity", "click");
+        }
+    };
+
+    private class TouchThread extends Thread
+    {
+        @Override
+        public void run() {
+            super.run();
+            while(isBtnDown)
+            {
+//                touchHandler.sendEmptyMessage(9876);
+                socket.emit("gobackToServer", 3);
+                try{
+                    Thread.sleep(200);
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void onBtnUp(){
+        TouchThread2 kThread = new TouchThread2();
+        kThread.start();
+    }
+    private Handler touchHandler2 = new Handler()
+    {
+        public void handleMessage(Message msg)
+        {
+//            Log.d("MainActivity", "click");
+        }
+    };
+
+    private class TouchThread2 extends Thread
+    {
+        @Override
+        public void run() {
+            super.run();
+            while(isBtnUp)
+            {
+//                touchHandler.sendEmptyMessage(9876);
+                socket.emit("gostraightToServer", 2);
+                try{
+                    Thread.sleep(200);
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void onBtnLeft(){
+        TouchThread3 kThread = new TouchThread3();
+        kThread.start();
+    }
+    private Handler touchHandler3 = new Handler()
+    {
+        public void handleMessage(Message msg)
+        {
+//            Log.d("MainActivity", "click");
+        }
+    };
+
+    private class TouchThread3 extends Thread
+    {
+        @Override
+        public void run() {
+            super.run();
+            while(isBtnLeft)
+            {
+//                touchHandler.sendEmptyMessage(9876);
+                socket.emit("turnleftToServer", 1);
+                try{
+                    Thread.sleep(200);
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void onBtnRight(){
+        TouchThread4 kThread = new TouchThread4();
+        kThread.start();
+    }
+    private Handler touchHandler4 = new Handler()
+    {
+        public void handleMessage(Message msg)
+        {
+//            Log.d("MainActivity", "click");
+        }
+    };
+
+    private class TouchThread4 extends Thread
+    {
+        @Override
+        public void run() {
+            super.run();
+            while(isBtnRight)
+            {
+//                touchHandler.sendEmptyMessage(9876);
+                socket.emit("turnrightToServer", 4);
+                try{
+                    Thread.sleep(200);
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
     // Touch 이벤트
     ImageButton.OnTouchListener onTouchListener = new View.OnTouchListener(){
         @Override
         public boolean onTouch(View view, MotionEvent motionEvent) {
             switch (motionEvent.getAction()){
                 case MotionEvent.ACTION_DOWN:   // 버튼이 눌렸을 때
-                case MotionEvent.ACTION_MOVE:   // 버튼이 눌려져 있을 때
+//                case MotionEvent.ACTION_MOVE:   // 버튼이 눌려져 있을 때
 //                  view.setBackgroundColor(Color.parseColor("#757575"));
 //                  왼-1/앞-2/뒤-3/오-4
                     if(view == btnUp){
-                        socket.emit("gostraightToServer", 2);
-                        Log.v("click", "up");
+                        isBtnUp = true;
+                        onBtnUp();
+//                        socket.emit("gostraightToServer", 2);
+//                        Log.v("click", "up");
                     }
                     else if(view == btnDown){
-                        socket.emit("gobackToServer", 3);
-                        Log.v("click", "down");
+                        isBtnDown = true;
+                        onBtnDown();
+//                        socket.emit("gobackToServer", 3);
+//                        Log.v("click", "down");
                     }
                     else if(view == btnLeft){
-                        socket.emit("turnleftToServer", 1);
-                        Log.v("click", "left");
+                        isBtnLeft = true;
+                        onBtnLeft();
+//                        socket.emit("turnleftToServer", 1);
+//                        Log.v("click", "left");
                     }
                     else if(view == btnRight){
-                        socket.emit("turnrightToServer", 4);
-                        Log.v("click", "right");
+                        isBtnRight = true;
+                        onBtnRight();
+//                        socket.emit("turnrightToServer", 4);
+//                        Log.v("click", "right");
                     }
                     break;
                 case MotionEvent.ACTION_UP:     // 버튼 뗄 때
                     view.setBackgroundColor(Color.TRANSPARENT);
+                    if(view == btnUp){
+                        isBtnUp = false;
+//                        socket.emit("gostraightToServer", 2);
+//                        Log.v("click", "up");
+                    }
+                    else if(view == btnDown){
+                        isBtnDown = false;
+
+//                        socket.emit("gobackToServer", 3);
+//                        Log.v("click", "down");
+                    }
+                    else if(view == btnLeft){
+                        isBtnLeft = false;
+
+//                        socket.emit("turnleftToServer", 1);
+//                        Log.v("click", "left");
+                    }
+                    else if(view == btnRight){
+                        isBtnRight = false;
+
+//                        socket.emit("turnrightToServer", 4);
+//                        Log.v("click", "right");
+                    }
                     break;
             }
             return false;
